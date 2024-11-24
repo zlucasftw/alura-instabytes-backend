9aa9c0fc6bbc2a31d356fcae02584eea7dbe98a7
zlucasftw <ilucasmcpvp@gmail.com>
2024-11-24 01:48:56 -0300

    docs: Aula 02 - Imersao Dev Back-End - Criando e Estruturando API com GET e Banco de Dados

--- server.js
+++ server.js
@@ -1,10 +1,78 @@
 import express from "express";
 
+const posts = [
+    {
+        id: 1,
+        descricao: "Uma foto teste",
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id: 2,
+        descricao: "Gato fazendo yoga",
+        /* imagem: "https://placekitten.com/400/300" */
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id: 3,
+        descricao: "Gatinho dormindo",
+        /* imagem: "https://placekitten.com/200/200" */
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id: 4,
+        descricao: "Gato explorando a caixa",
+        /* imagem: "https://placekitten.com/300/200" */
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id: 5,
+        descricao: "Gato com um brinquedo",
+        /* imagem: "https://placekitten.com/400/300" */
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id: 6,
+        descricao: "Gato olhando pela janela",
+        /* imagem: "https://placekitten.com/200/200" */
+        imagem: "https://placecats.com/millie/300/150"
+    }
+]
+
+function buscarPostPorID(id) {
+    /* return posts.findIndex((post) => {
+        return post.id === Number(id);
+    }); */
+
+    if (id.trim() === "" || id === null) {
+        return false;
+    }
+
+    try {
+        id = Number(id);
+    } catch (error) {
+        return false;
+    }
+
+    return posts.filter(((post) => post.id === id));
+}
+
 const app = express();
+app.use(express.json());
 
-app.get("/api", (req, res) => {
-    res.status(200).send("A Torre Eiffel iluminada à noite, com milhares de luzes cintilando, criando um espetáculo mágico em Paris.");
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts);
+});
 
+app.get("/posts/:id", (req, res) => {
+    const index = buscarPostPorID(req.params.id);
+
+    if (index === false) {
+        res.sendStatus(400);
+    } else {
+        res.status(200).json(index);
+    }
+    /* res.status(200).json(posts[index]); */
+    
 });
 
 app.listen(3000, () => {
